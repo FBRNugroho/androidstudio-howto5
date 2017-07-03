72b634f7ee4d80b920b0a1be3ff628fec7168edf
Febrianto Nugroho <id@febriantonugroho.com>
2017-07-03 13:28:01 +0700

    2017-07-03

---
+++ app/src/main/java/com/febriantonugroho/androidstudio_howto5/MainActivity.java
@@ -0,0 +1,23 @@
+package com.febriantonugroho.androidstudio_howto5;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.febriantonugroho.com")));
+            }
+        });
+    }
+}
